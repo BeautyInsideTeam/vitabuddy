cadd5f04f4e4dcb5f23d1afae4102f2295c3824c
SeongYeon Han <cicle1999@gmail.com>
2024-10-11 11:32:24 +0900

    supplementList js 추가(카테고리,검색,페이지), jsp에 script 추가

--- js/search.js
+++ js/search.js
@@ -2,65 +2,80 @@
  * 
  */
  
-$(document).ready(function() {
-    $('#searchForm').on('submit', function(event) {
+ $(document).ready(function() {
+    // 검색 폼 submit
+    $('#searchForm').on('submit', function() {
         event.preventDefault();
 
-        const keyword = $('#keyword').val();
-        applyFilters(keyword);
+        const keyword = $('#keyword').val(); //검색 키워드
+        applyFilters(keyword, null, null);
     });
 
-    // 하위 카테고리
-    $('#subCtg .subCtgMenu li a').on('click', function(event) {
+
+    // 하위 카테고리 클릭
+    $('#subCtg').on('click', '.subCtgMenu li a', function() {
         event.preventDefault();
-        
-        const categoryType = $(this).text();  // 선택한 하위 카테고리 값
 
-        applyFilters(null, categoryType);
+        const categoryType = $(this).text();  // 선택한 하위 카테고리
+        applyFilters(null, categoryType, null);
     });
 
-    // 정렬
+    // 정렬 선택
     $('#order').on('change', function() {
-        const selectedOrder = $(this).val();  // 정렬 선택
+        const selectedOrder = $(this).val();  // 정렬 값
         applyFilters(null, null, selectedOrder);
     });
 
+    // 필터링
     function applyFilters(keyword, categoryType, sortOrder) {
         const url = new URL(window.location.href);
 
-        // 필터링 파라미터
-        if (keyword !== null) {
+        // 기존 파라미터 삭제/추가
+        if (keyword !== null) { 
             url.searchParams.set('keyword', keyword);
+        } else {
+            url.searchParams.delete('keyword');
         }
+
         if (categoryType !== null) {
             url.searchParams.set('ctg', categoryType);
+        } else {
+            url.searchParams.delete('ctg');
         }
+
         if (sortOrder !== null) {
             url.searchParams.set('order', sortOrder);
+        } else {
+            url.searchParams.delete('order');
         }
 
         // ajax 요청
         $.ajax({
             type: 'get',
-            url: '/api/products?' + url.searchParams.toString(),
+            url: '/api/supplement?' + url.searchParams.toString(),
             success: function(result) {
-                $('.products').empty(); // 기존 태그 삭제
-                
-                if (result.length == 0) { // 검색 결과 없는 경우
+                $('.products').empty();  // 기존 상품 목록 삭제
+
+                if (result.length == 0) {
                     $('.products').append('<h4>검색 결과가 없습니다</h4>');
-                } else { // 검색 결과 있는 경우
-                    for (let i = 0; i < result.length; i++) {
-                        $('.products').append('<div class="productItem"><a href="/product/detailView/' + result[i].supNo + '">' +
-                            '<img src="/image/' + result[i].supImg + '.png"></a>' +
-                            '<p>' + result[i].supName + '</p>' +
-                            '<p>' + result[i].supPrice + '</p>' +
-                            '<p>' + result[i].supBrand + '</p></div>';
-                    } // for
-                } // else
+                } else {
+                    result.forEach(function(product) {
+                        $('.products').append(`
+                            <div class="productItem">
+					            <a href="<c:url value='/api/supplement/supplementDetail/${sup.supId}'/>">
+					            <img src="${sup.supImg}" alt="${sup.supName}">
+						        </a>
+						        <p>${sup.supName}</p>
+						        <p>${sup.supPrice}</p>
+						        <p>${sup.supBrand}</p>
+                            </div>
+                        `);
+                    });
+                }
             },
             error: function() {
-                alert("요청 실패");
+                console.error("error");
             }
         });
-    } // applyFilters
+    }
 });
--- views/product/supplementList.jsp
+++ views/product/supplementList.jsp
@@ -12,6 +12,7 @@
 <script src="<c:url value='/js/jquery-3.7.1.min.js'/>"></script>
 <script src="<c:url value='/js/category.js'/>"></script>
 <!-- 검색 js 추가 --><script src="<c:url value='/js/search.js'/>"></script>
+<!-- 페이지 js 추가 --><script src="<c:url value='/js/pageSupplementList.js'/>"></script>
 <!-- css 링크 수정 --><link rel="stylesheet" type="text/css" href="<c:url value='/css/supplementList.css'/>">
 
 </head>
